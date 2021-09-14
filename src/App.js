6b01d84db21033579a6521b2b13377c45f21db0e
Bryan Hain <bryanhain97@gmail.com>
2021-09-14 16:31:12 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Searchbar from './components/Searchbar/Searchbar';
+
 function App() {
 
   return (
     <div className="app">
       <Navbar />
+      <Searchbar />
       <main className="main-section">
       </main>
     </div>
